a30d9ba16b5152132d3971e19d877f82a4600a6d
Ayamarmash <ayamarmash74@gmail.com>
2024-02-29 12:57:47 +0200

    add default pic for companies that don't have logo

--- src/shared_components/card/card.js
+++ src/shared_components/card/card.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './card.css'
+import defaultCardLogo from '../../assets/default_card_logo.png'
 import SimpleSlider from "../slider/slider";
 
 function Card (props) {
@@ -41,7 +42,7 @@ function Card (props) {
         return (
             <div className="card">
                     <div className="head">
-                            <img className="card-logo" src={props.logo} alt="card-logo"/>
+                            <img className="card-logo" src={props.logo || defaultCardLogo} alt="card-logo"/>
                             <div className="card-description">
                         <div className="company-name">{props.company_name}</div>
                         <div className="company-slogan">{props.slogan}</div>
